c357de2f1ac294e1d3d0239e94e8a94ed9755bdd
mauriziopalmisano <maurizio.palmisano1@gmail.com>
2026-04-30 15:22:16 +0200

    creazione del componente CardLanguage con map e passando gli attributi tramite props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import CardLanguage from "./components/CardLanguage"
 import languages from "./data/languages"
-import { useState } from "react"
+
 
 
 
 function App() {
-  
+
+
   return (
     <>
-      
+      <div className="container">
+        {languages.map(language => {
+          const { id, title, description } = language;
+          return <CardLanguage
+            id={id}
+            title={title}
+            description={description}
+          />
+        })}
+      </div>
     </>
   )
 }
--- src/components/CardLanguage.jsx
+++ src/components/CardLanguage.jsx
@@ -1,11 +1,21 @@
-function CardLanguage() 
+function CardLanguage({
+  id = 0,
+  title = 'Titolo',
+  description = 'Descrizione'
+}) {
 
 
 
-{
   return (
     <>
-
+    <div className="row" key={id}>
+      <div class="card col my-3">
+        <div class="card-body">
+          <button className="btn btn-primary">{title}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
