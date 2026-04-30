f09d863466181b94214bcbe2d54974e273e296cd
mauriziopalmisano <maurizio.palmisano1@gmail.com>
2026-04-30 15:52:18 +0200

    aggiunta una variabile di stato per gestire se far vedere la descrizione

--- src/components/CardLanguage.jsx
+++ src/components/CardLanguage.jsx
@@ -1,18 +1,27 @@
+import { useState } from "react"
+
+
 function CardLanguage({
   id = 0,
   title = 'Titolo',
   description = 'Descrizione'
 }) {
 
+  const [show, setShow] = useState(false)
 
+  const clickListener = () => {
+    setShow(!show)
+  }
+
+  const btnClass = (show ? 'btn-warning' : 'btn-primary')
 
   return (
     <>
     <div className="row" key={id}>
       <div class="card col my-3">
         <div class="card-body">
-          <button className="btn btn-primary">{title}</button>
-          <p>{description}</p>
+          <button onClick={clickListener} className={`btn ${btnClass} mb-3`}>{title}</button>
+          {show && (<p>{description}</p>)}
         </div>
       </div>
     </div>
